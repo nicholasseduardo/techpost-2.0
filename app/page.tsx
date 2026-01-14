3e75fb67c7909845872c2262bc5bfc7e1970d74f
nicholasseduardo <nicholas.seduardo@gmail.com>
2026-01-14 15:51:31 -0300

    Bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -420,7 +420,7 @@ const App: React.FC = () => {
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mb-8">
             
             {/* COLUNA DA ESQUERDA: SELETORES (Ocupa 5/12 do espaço) */}
-            <div className="lg:col-span-5 space-y-6">
+            <div className="lg:col-span-5 space-y-2">
                 
                 {/* Canal */}
                 <div className="space-y-2">
